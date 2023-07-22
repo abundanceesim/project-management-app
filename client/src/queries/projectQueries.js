6800134550509921ae6c752f027dc50be903534f
Abundance Esim <abundanceesim@gmail.com>
2023-07-22 02:40:36 -0400

    Add router navigation and implement mutations

--- client/src/queries/projectQueries.js
+++ client/src/queries/projectQueries.js
@@ -9,5 +9,21 @@ const GET_PROJECTS = gql`
         }
     }
 `
+const GET_PROJECT = gql`
+  query getProject($id: ID!) {
+    project(id: $id) {
+      id
+      name
+      description
+      status
+      client {
+        id
+        name
+        email
+        phone
+      }
+    }
+  }
+`;
 
-export { GET_PROJECTS}
+export { GET_PROJECTS, GET_PROJECT };
